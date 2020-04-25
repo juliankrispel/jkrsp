00319eb9561496fbc4482f4a62e892a6c037e73c
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-25 17:12:14 +0100

    what

--- main.tf
+++ main.tf
@@ -16,18 +16,18 @@ resource "aws_s3_bucket" "b" {
   acl    = "public-read"
 
   policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::*/*"
-        }
-      ]
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::*/*"
     }
+  ]
+}
   POLICY
 
   website {
