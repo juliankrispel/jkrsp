c9b481949d306e9f40c86faeb28a6ff20b965929
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-25 17:07:27 +0100

    ffs

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket" "b" {
           "Effect": "Allow",
           "Principal": "*",
           "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.env_prefix}jkrsp.com/*"
+          "Resource": "arn:aws:s3:::*/*"
         }
       ]
     }
