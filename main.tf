b2984beb40d93c53214be6544d59b0458cb9fb1b
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-26 11:05:57 +0100

    correct var

--- main.tf
+++ main.tf
@@ -43,6 +43,6 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-output "bucket_domain_name" {
-  value = aws_s3_bucket.b.bucket_domain_name
+output "website" {
+  value = aws_s3_bucket.b.website_endpoint
 }
