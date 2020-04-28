8b261679ff49882017f35e11389a238823b1d575
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-28 10:57:19 +0100

    update workflow

--- .github/actions/comment.js
+++ .github/actions/comment.js
@@ -8,13 +8,15 @@ async function run() {
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const { issue_number, github_token } = process.env
+  const { issue_number, github_token, owner, repo } = process.env
 
   const octokit = new github.GitHub(github_token);
 
   const { data } = await octokit.issues.createComment({
-      issue_number,
-      body: "hello"
+    owner,
+    repo,
+    issue_number,
+    body: "hello"
   });
 
   console.log(data);
