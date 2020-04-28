46516f3ff1415f6cabf16cbfc09b381c9f134d30
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-28 11:23:22 +0100

    update workflow

--- .github/actions/comment.js
+++ .github/actions/comment.js
@@ -8,7 +8,7 @@ async function run() {
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const { issue_number, github_token, owner, repo } = process.env
+  const { issue_number, github_token, owner, repo, website_link } = process.env
 
   const octokit = new github.GitHub(github_token);
 
@@ -16,7 +16,7 @@ async function run() {
     owner,
     repo,
     issue_number,
-    body: "hello"
+    body: `[Review website here](${website_link.replace(/"/gs, '')})`
   });
 
   console.log(data);
