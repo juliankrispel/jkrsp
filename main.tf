7a3e792e4b6c2721df0d6b3be1ba817fc49252b7
Julian Krispel <julian@jkrsp.com>
2025-01-07 10:19:48 +0000

    update teraform

--- main.tf
+++ main.tf
@@ -16,33 +16,46 @@ variable "is_temp_env" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "${var.env_prefix}jkrsp.com"
-  acl    = "public-read"
   force_destroy = var.is_temp_env
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${var.env_prefix}jkrsp.com/*"
-    }
-  ]
-}
-  POLICY
-
-  website {
-    index_document = "index.html"
-  }
 
   tags = {
     ManagedBy = "terraform"
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "b" {
+  bucket = aws_s3_bucket.b.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "b" {
+  bucket = aws_s3_bucket.b.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.b.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${var.env_prefix}jkrsp.com/*"
+      }
+    ]
+  })
+}
+
 output "website" {
-  value = "http://${aws_s3_bucket.b.website_endpoint}"
+  value = "http://${aws_s3_bucket.b.bucket_regional_domain_name}"
 }
