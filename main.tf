251755821d035a04d77d99f4e1612c4fd67fcc81
Julian Krispel <julian@jkrsp.com>
2025-01-07 10:40:10 +0000

    launch

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
-provider "aws" {
-  region = "eu-west-2"
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
   backend "s3" {
     bucket = "jkrsp-tf-state"
     region = "eu-west-2"
   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
 }
 
 variable "env_prefix" { }
